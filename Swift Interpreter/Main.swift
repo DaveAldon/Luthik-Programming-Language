84fd0233f526e7c201907f057f45b5f022679c08
David Crawford <crawford@mail.gvsu.edu>
2016-08-30 15:38:45 -0400

    Array contents are now dumped into a public array to be easily passed to other classes for interpretation.

--- Swift Interpreter/Main.swift
+++ Swift Interpreter/Main.swift
@@ -1,6 +1,6 @@
 //
 //  Main.swift
-//  
+//
 //
 //  Created by David Crawford on 8/29/16.
 //
@@ -8,25 +8,30 @@
 
 import Foundation
 
-let file = "luthiksyntax.luthik";
+let file = "luthiksyntax.luthik"
+public var arrayLines = [String]()
 
-while (true) {
-    var input = "";
-    
-    do {
-        if let dir = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true).first {
-            let path = NSURL(fileURLWithPath: dir).URLByAppendingPathComponent(file)
-            
-            //Read the file and seperate the lines into an array, skipping blank lines
-            do {
-                let input = try String(contentsOfURL: path, encoding:NSUTF8StringEncoding)
-                let newlineChars = NSCharacterSet.newlineCharacterSet()
-                let lines = input.utf16.split { newlineChars.characterIsMember($0) }.flatMap(String.init)
-                print(lines)
+do {
+    if let dir = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true).first {
+        let path = NSURL(fileURLWithPath: dir).URLByAppendingPathComponent(file)
+        
+        //Read the file and seperate the lines into an array, skipping blank lines
+        do {
+            let input = try String(contentsOfURL: path, encoding:NSUTF8StringEncoding)
+            let newlineChars = NSCharacterSet.newlineCharacterSet()
+            let lines = input.utf16.split { newlineChars.characterIsMember($0) }.flatMap(String.init)
+            var linesLength = lines.count
+
+            //Loop through the array and place its values into a public array
+            for i in 1..<linesLength {
+                arrayLines.append(lines[i])
             }
-            catch {
-                print("No file in path")
-            }
+        }
+        catch {
+            print("No file in path")
         }
     }
 }
+
+//Check out what's inside our public array
+print(arrayLines)
